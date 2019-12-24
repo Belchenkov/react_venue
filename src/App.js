026ee04b8cd007a06f891cd924a9a1affd670777
Aleksey Belchenkov <u608110@gmail.com>
2019-12-24 23:45:39 +0300

    Creating the Header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
+import Header from "./components/header_footer/Header";
+
+import './resources/styles.css';
+
 function App() {
   return (
     <div className="App">
-
+      <Header />
     </div>
   );
 }
