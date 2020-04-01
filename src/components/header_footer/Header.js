5bfcd2df8bc7aa1326d33b23ed896b2dafcf0e22
Aleksey Belchenkov <u608110@gmail.com>
2020-04-01 12:26:06 +0300

    Finishing the Header

--- src/components/header_footer/Header.js
+++ src/components/header_footer/Header.js
@@ -8,15 +8,31 @@ import MenuIcon from '@material-ui/icons/Menu';
 import SideDrawer from "./SideDrawer";
 
 class Header extends Component {
-
     state = {
         drawerOpen: false
     };
 
     toggleDrawer = (value) => {
         this.setState({
-            drawerOpen: value
+            drawerOpen: value,
+            headerShow: false
         })
+    };
+
+    componentDidMount() {
+        window.addEventListener('scroll', this.handleScroll);
+    }
+
+    handleScroll = () => {
+        if (window.scrollY > 0) {
+            this.setState({
+                headerShow: true
+            });
+        } else {
+            this.setState({
+                headerShow: false
+            });
+        }
     };
 
     render() {
@@ -24,7 +40,7 @@ class Header extends Component {
             <AppBar
                 position="fixed"
                 style={{
-                    backgroundColor: '#2f2f2f',
+                    backgroundColor: this.state.headerShow ? '#2f2f2f' : 'transparent',
                     boxShadow: 'none',
                     padding: '10px 0'
                 }}
