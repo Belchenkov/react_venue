f914bf1e546b71f9980724311aeaf8cb0b0929d5
Aleksey Belchenkov <u608110@gmail.com>
2020-04-01 12:42:48 +0300

    Slider section

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Header from "./components/header_footer/Header";
+import Featured from "./components/featured";
 
 import './resources/styles.css';
 
@@ -8,6 +9,7 @@ function App() {
   return (
     <div className="App" style={{ height: "1500px", background: 'cornflowerblue' }}>
       <Header />
+      <Featured />
     </div>
   );
 }
